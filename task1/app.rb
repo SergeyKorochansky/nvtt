2ed30e10f763217316c2f429b4a332246681317c
Serhij Korocahnskyj <webzepter@gmail.com>
2015-01-21 16:14:17 +0200

    Fix class names

--- task1/app.rb
+++ task1/app.rb
@@ -4,64 +4,61 @@ require 'sinatra/asset_pipeline'
 require 'bootstrap-sass'
 require_relative 'user'
 
-class App < Sinatra::Base
+configure do
+  enable :sessions
+  set :sessions, httponly: true, secure: production?, expire_after: 60*60, secret: ENV['SESSION_SECRET']
 
-  configure do
-    enable :sessions
-    set :sessions, httponly: true, secure: production?, expire_after: 60*60, secret: ENV['SESSION_SECRET']
+  use Rack::Deflater
+  register Sinatra::AssetPipeline
+end
 
-    use Rack::Deflater
-    register Sinatra::AssetPipeline
+get '/login' do
+  if logged_in?
+    redirect :profile, flash[:notice] = 'You are already logged in!'
+  else
+    @title = 'Login'
+    haml :login
   end
+end
 
-  get '/login' do
-    if logged_in?
-      redirect :profile, flash[:notice] = 'You are already logged in!'
-    else
-      @title = 'Login'
-      haml :login
-    end
-  end
-
-  post '/login' do
-    if logged_in?
-      redirect :profile, flash[:notice] = 'You are already logged in!'
-    else
-      user = User.new(params[:name], params[:password])
-      if user && user.can_login?
-        if user.correct_password?
-          user.unsuccessful_tries = 0
-          user.save
-          session[:user_name] = user.name
-          redirect :profile, flash[:notice] = "Hello!, #{user.name}"
-        else
-          user.unsuccessful_tries += 1
-          user.last_incorrect_login_try_at = Time.now.to_i
-          user.save
-          redirect :login, flash[:error] = 'Incorrect credentials'
-        end
+post '/login' do
+  if logged_in?
+    redirect :profile, flash[:notice] = 'You are already logged in!'
+  else
+    user = User.new(params[:name], params[:password])
+    if user && user.can_login?
+      if user.correct_password?
+        user.unsuccessful_tries = 0
+        user.save
+        session[:user_name] = user.name
+        redirect :profile, flash[:notice] = "Hello!, #{user.name}"
       else
-        redirect :login, flash[:error] = "Try again after #{user.can_login_after} seconds"
+        user.unsuccessful_tries += 1
+        user.last_incorrect_login_try_at = Time.now.to_i
+        user.save
+        redirect :login, flash[:error] = 'Incorrect credentials'
       end
-    end
-  end
-
-  post '/logout' do
-    session[:user_name] = nil
-    redirect :login
-  end
-
-  get '/profile' do
-    if logged_in?
-      @title = 'Profile'
-      @user_name = session[:user_name]
-      haml :profile
     else
-      redirect :login, flash[:notice] = 'You must be logged in'
+      redirect :login, flash[:error] = "Try again after #{user.can_login_after} seconds"
     end
   end
+end
 
-  def logged_in?
-    !session[:user_name].nil?
+post '/logout' do
+  session[:user_name] = nil
+  redirect :login
+end
+
+get '/profile' do
+  if logged_in?
+    @title = 'Profile'
+    @user_name = session[:user_name]
+    haml :profile
+  else
+    redirect :login, flash[:notice] = 'You must be logged in'
   end
+end
+
+def logged_in?
+  !session[:user_name].nil?
 end
